fcdddeff3abefc46ee8e308c8548f191083450bd
mutoulbj <flt0426@gmail.com>
2014-04-11 01:31:22 +0800

    initial project wf_blog

---
+++ wf_blog/README.txt
@@ -0,0 +1,14 @@
+wf_blog README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/python setup.py develop
+
+- $VENV/bin/initialize_wf_blog_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ wf_blog/setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='wf_blog',
+      version='0.0',
+      description='wf_blog',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='wf_blog',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = wf_blog:main
+      [console_scripts]
+      initialize_wf_blog_db = wf_blog.scripts.initializedb:main
+      """,
+      )
